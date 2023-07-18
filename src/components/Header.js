cf310abe84d23f860a5c96251746e9216bb03409
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2023-07-18 19:27:53 +0200

    one header

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ const [fade, setFade] = useState("opacity-0");
 
 const router = useRouter();
 const query = router.query;
-
+console.log(query);
 let paths = [];
 const nav = navigation.data.links;
 navigation.data.links.forEach((e, i) => {
@@ -29,20 +29,28 @@ setTimeout(() => {
 
   return (
     <>
-      <header className={`max-md:hidden z-[10] top-0 left-0 right-0 ${fade}`}>
+      <header
+        className={`${
+          router?.asPath === "/" && "absolute"
+        } max-md:hidden z-[10] top-0 left-0 right-0 ${fade}`}
+      >
         <div className="relative py-[2.5vw] px-[4vw] flex flex-wrap items-center justify-between gap-x-6 gap-y-3 leading-none">
           <PrismicNextLink
             href="/"
-            className="tracking-normal max-md:text-headerTitleMobile md:text-headerTitle font-serif font-normal leading-[2.8rem] uppercase text-black"
+            className={`tracking-normal max-md:text-headerTitleMobile md:text-headerTitle font-serif font-normal leading-[2.8rem] uppercase ${
+              router?.asPath === "/" ? "text-white" : "text-black"
+            }`}
           >
             <PrismicText field={settings.data.siteTitle} />
           </PrismicNextLink>
           <nav>
             <ul className="flex flex-wrap items-center">
-              {navigation.data?.links.map((item,index) => (
+              {navigation.data?.links.map((item, index) => (
                 <li
                   key={prismic.asText(item.label)}
-                  className="tracking-normal text-slate-800 ml-[1.3vw] font-poppins font-light text-black"
+                  className={`tracking-normal text-slate-800 ml-[1.3vw] font-poppins font-light ${
+                    router?.asPath === "/" ? "text-white" : "text-black"
+                  }`}
                 >
                   <PrismicNextLink
                     field={item.link}
@@ -68,7 +76,9 @@ setTimeout(() => {
                       beforeInjection={(svg) => {
                         svg.classList.add("headerIcon");
                       }}
-                      className="fill-black"
+                      className={`${
+                        router?.asPath === "/" ? "fill-white" : "fill-black"
+                      }`}
                     />
                   </PrismicNextLink>
                 </li>
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,28 +1,10 @@
-import { useEffect, useState } from "react";
-import { HeaderFrontpage } from "./HeaderFrontpage";
-import { usePathname } from "next/navigation";
 import { Header } from "./Header";
 
 export function Layout({ navigation, settings, children }) {
-  const [navbar, setNavbar] = useState(null)
-  const pathname = usePathname();
-
-  useEffect(() => {
-    if(pathname === "/") {
-      setNavbar(true)
-    } else {
-      setNavbar(false)
-    }
-  },[pathname])
-
-
+ 
   return (
     <div className="text-slate-800">
-      {navbar ? (
-        <HeaderFrontpage navigation={navigation} settings={settings} />
-      ) : (
         <Header navigation={navigation} settings={settings} />
-      )}
       <main>{children}</main>
     </div>
   );
